3068d1ff4160aa51fe285c67a9bef47ff55c2d9e
mikenic <temperanceaubie@gmail.com>
2017-10-11 23:14:22 -0700

    image in play show page

--- app/models/play.rb
+++ app/models/play.rb
@@ -2,6 +2,6 @@ class Play < ApplicationRecord
 	belongs_to :user
 	belongs_to :category
 
-	has_attached_file :play_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :play_img, styles: { medium: "250x350>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end
